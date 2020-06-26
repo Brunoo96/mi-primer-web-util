f48e44ec4ccbdce96487c429768eca3df5ea99a7
br1valle <brunovalle@users.noreply.github.com>
2020-06-26 09:05:38 -0300

    primeros pasos en js

---
+++ resources/01-js.js
@@ -0,0 +1,5 @@
+/*
+alert("bienvenido perro");
+*/
+console.log("bienvenido a la consola")
+console.log(5+5)
---
+++ resources/02-variables.js
@@ -0,0 +1,14 @@
+"strict mode"
+
+var numero1 = 12
+var numero2 = 200
+var suma = numero1 + numero2
+
+console.log(numero1 + numero2)
+
+
+if(numero1 > numero2 || numero2 < 100){
+    console.log("asdasdasd")
+}else{
+    console.log("la suma és: " + suma)
+}
